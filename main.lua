91ece656a863ba38b96055034b1ed808cb9bf77d
tnc47 <thanachok7099@gmail.com>
2025-09-26 20:32:24 +0700

    Add functionality to claim pet coins in the game

---
+++ main.lua
@@ -0,0 +1,4 @@
+local Eggs = loadstring(game:HttpGet("https://raw.githubusercontent.com/tnc47/B.A.Z/refs/heads/main/modules/Eggs.lua"))()
+local Pets = loadstring(game:HttpGet("https://raw.githubusercontent.com/tnc47/B.A.Z/refs/heads/main/modules/Pets.lua"))()
+
+Pets:claimPetCoins()
