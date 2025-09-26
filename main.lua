24022d4c52adab714476f43561f3849074520903
Thanachok <104304933+tnc47@users.noreply.github.com>
2025-09-26 21:02:57 +0700

    Add DumpTable function and print statements for Eggs, Pets, and Island modules

--- main.lua
+++ main.lua
@@ -1,4 +1,41 @@
 local Eggs = loadstring(game:HttpGet("https://raw.githubusercontent.com/tnc47/B.A.Z/refs/heads/main/modules/Eggs.lua"))()
 local Pets = loadstring(game:HttpGet("https://raw.githubusercontent.com/tnc47/B.A.Z/refs/heads/main/modules/Pets.lua"))()
+local Island = loadstring(game:HttpGet("https://raw.githubusercontent.com/tnc47/B.A.Z/refs/heads/main/modules/Island.lua"))()
 
-Pets:claimPetCoins()
+
+function DumpTable(table, nb)
+    if nb == nil then
+        nb = 0
+    end
+
+    if type(table) == "table" then
+        local s = ""
+        for _ = 1, nb + 1, 1 do
+            s = s .. "    "
+        end
+
+        s = "{\n"
+        for k, v in pairs(table) do
+            if type(k) ~= "number" then
+                k = '"' .. k .. '"'
+            end
+            for _ = 1, nb, 1 do
+                s = s .. "    "
+            end
+            s = s .. "[" .. k .. "] = " .. DumpTable(v, nb + 1) .. ",\n"
+        end
+
+        for _ = 1, nb, 1 do
+            s = s .. "    "
+        end
+
+        return s .. "}"
+    else
+        return tostring(table)
+    end
+end
+
+
+print("Eggs:", DumpTable(Eggs))
+print("Pets:", DumpTable(Pets))
+print("Island:", DumpTable(Island))
